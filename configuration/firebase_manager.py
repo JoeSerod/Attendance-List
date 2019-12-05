f39a6a3565651c9275fa77549e466b2db35f671e
CamiloSegovia <54286208+CamiloSegovia@users.noreply.github.com>
2019-12-05 17:15:57 -0600

    create_worker.html

--- configuration/firebase_manager.py
+++ configuration/firebase_manager.py
@@ -1,5 +1,6 @@
 from firebase_admin import credentials, firestore, initialize_app
 
+
 class FireStoreService:
     """
     This class should handle reading & writing from and to firebase.
@@ -9,7 +10,7 @@ class FireStoreService:
         cred = credentials.Certificate('attendance-list.json')
         default_app = initialize_app(cred)
         self.db = firestore.client()
-        self.student_ref = self.db.collection('students')
+        self.worker_ref = self.db.collection('worker')
 
     def create_worker(self,json):
         self.worker_ref.document(json['id']).set(json)
--- list.py
+++ list.py
@@ -1,4 +1,4 @@
-
+import os
 import requests
 from models.response import Response
 from flask import Flask, request, jsonify, render_template
@@ -7,9 +7,30 @@ from configuration.firebase_manager import FireStoreService
 app = Flask(__name__)
 fb_service = FireStoreService()
 
-
-@app.route('/api/worker/create')
+@app.route('/create/worker', methods= ["GET","POST"])
 def create_worker():
+    if request.method == 'GET':
+        res = requests.get("http://localhost:5000/api/category/select",
+                           headers=headers).json()
+        return render_template("create_product.html", categories=res['data'])
+    if request.method == 'POST':
+        form = request.form
+        new_product = Product(form['id'], form['name'], form['details'],
+                              form['price'], form['quantity'], form['stock'],
+                              request.form.get('select_category'),
+                              form['image'])
+        print(new_product)
+        res = requests.post("http://localhost:5000/api/product/create",
+                            json=new_product.__dict__, headers=headers).json()
+        print("res req: ", res)
+        if res['status'] == 200:
+            return redirect(url_for('index'))  # redirect to index
+        else:
+            return jsonify(res), 500  # returns json error
+
+@app.route('/api/worker/create', methods=["POST"])
+def api_create_worker():
+
     try:
         data = request.json
         fb_service.create_worker(data)
@@ -20,9 +41,6 @@ def create_worker():
         return jsonify(res.__dict__), 500
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
-
+port = int(os.environ.get('PORT', 5000))
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded=True, host='localhost', port=port)
