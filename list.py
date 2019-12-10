052e53503c63e88e8db120cda5c0a70f054f33b6
FerQuezada <fer_quezada_@outlook.es>
2019-12-09 18:35:35 -0600

    commit

--- list.py
+++ list.py
@@ -1,10 +1,27 @@
+from flask import Flask, request, render_template, jsonify, redirect, url_for
+import os
+
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/', methods=['GET'])
+def index():
+    """
+    View for the landing page.
+    :return: redirection to product listings.
 
+    return redirect(url_for('product_select_list'))
+    """
+    return redirect(url_for('product_select_list'))
+
+
+@app.route('/product/list', methods=['GET'])
+def product_select_list():
+    # if request.method == 'GET':
+    return render_template('hola_mundo.html')
+
+
+port = int(os.environ.get('PORT', 5000))
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded=True, host='localhost', port=port)
