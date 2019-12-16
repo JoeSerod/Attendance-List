a1e62b63b43eb98345f4347934b65cc61da40c33
CamiloSegovia <54286208+CamiloSegovia@users.noreply.github.com>
2019-12-16 14:27:51 -0600

    user_models

---
+++ models/user.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, name, last_name, phone_number, e_mail, password):
+
+        self.phone_number = phone_number
+        self.name = name
+        self.last_name = last_name
+        self.e_mail = e_mail
+        self.password = password
